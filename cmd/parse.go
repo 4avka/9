fd6f2c27247e712d536c0bedbdc75c9de4b27f72
Loki Verloren <l0k18@protonmail.com>
2019-03-26 10:41:54 +0100

    starting parse

--- cmd/parse.go
+++ cmd/parse.go
@@ -1,10 +1,24 @@
 package cmd
 
-import "time"
+import (
+	"time"
+
+	"git.parallelcoin.io/dev/9/pkg/util/cl"
+)
 
 func Parse(args []string) int {
 	// parse commandline
-
+	// collect set of items in commandline
+	if len(args) < 2 {
+		log <- cl.Info{"no args given, printing help"}
+	}
+	for i, x := range args[1:] {
+		for j, y := range commands {
+			if y.RE.Match([]byte(x)) {
+				log <- cl.Info{"found", i, j, x}
+			}
+		}
+	}
 	// read configuration
 	time.Sleep(time.Second)
 	return 0
