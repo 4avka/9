fd9f7e906437f70ac2115b373b03ecf65d83a971
Loki Verloren <l0k18@protonmail.com>
2019-03-26 20:12:03 +0100

    added skeleton handlers

---
+++ cmd/handlers.go
@@ -0,0 +1,45 @@
+package cmd
+
+func Help(c *Command) int {
+	return 0
+}
+
+func Conf(c *Command) int {
+	return 0
+}
+
+func New(c *Command) int {
+	return 0
+}
+
+func Copy(c *Command) int {
+	return 0
+}
+
+func List(c *Command) int {
+	return 0
+}
+
+func Ctl(c *Command) int {
+	return 0
+}
+
+func Node(c *Command) int {
+	return 0
+}
+
+func Wallet(c *Command) int {
+	return 0
+}
+
+func Shell(c *Command) int {
+	return 0
+}
+
+func Test(c *Command) int {
+	return 0
+}
+
+func Create(c *Command) int {
+	return 0
+}
--- cmd/parse.go
+++ cmd/parse.go
@@ -1,25 +1,43 @@
 package cmd
 
 import (
-	"time"
-
 	"git.parallelcoin.io/dev/9/pkg/util/cl"
 )
 
 func Parse(args []string) int {
 	// parse commandline
+	err := parseCLI(args)
+	if err != nil {
+		log <- cl.Error{err}
+	}
+	// read configuration
+
+	return 0
+}
+
+func parseCLI(args []string) error {
 	// collect set of items in commandline
 	if len(args) < 2 {
 		log <- cl.Info{"no args given, printing help"}
 	}
-	for i, x := range args[1:] {
-		for j, y := range commands {
-			if y.RE.Match([]byte(x)) {
-				log <- cl.Info{"found", i, j, x}
+	commandsFound := make(map[string]int)
+	for _, x := range args[1:] {
+		for _, y := range commandsList {
+			if commands[y].RE.Match([]byte(x)) {
+				if _, ok := commandsFound[y]; ok {
+					// log <- cl.Info{"found", i, y, x}
+					commandsFound[y]++
+					break
+				} else {
+					// log <- cl.Info{"found", i, y, x}
+					commandsFound[y] = 1
+					break
+				}
 			}
 		}
 	}
-	// read configuration
-	time.Sleep(time.Second)
-	return 0
+
+	log <- cl.Warn{args, ":", commandsFound}
+
+	return nil
 }
