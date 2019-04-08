c30c220e71a146d77237bc2eebdaec94dbaf9543
Loki Verloren <l0k18@protonmail.com>
2019-04-08 20:04:00 +0200

    ready to parse some thingies

--- cmd/tmp/validators.go
+++ cmd/tmp/validators.go
@@ -1,49 +1,247 @@
 package config
 
+import "time"
+
 var Valid = struct {
 	File, Dir, Port, Bool, Int, Tag, Tags, Addr, Addrs, Algo, Float,
 	Duration, Net, Level func(*Row, interface{}) bool
 }{
-	File: func(*Row, interface{}) bool {
+	File: func(r *Row, in interface{}) bool {
+		var s string
+		switch I := in.(type) {
+		case string:
+			s = I
+		case *string:
+			s = *I
+		default:
+		}
+		_ = s
 		return false
 	},
-	Dir: func(*Row, interface{}) bool {
+	Dir: func(r *Row, in interface{}) bool {
+		var s string
+		switch I := in.(type) {
+		case string:
+			s = I
+		case *string:
+			s = *I
+		default:
+		}
+		_ = s
 		return false
 	},
-	Port: func(*Row, interface{}) bool {
+	Port: func(r *Row, in interface{}) bool {
+		var s string
+		isString := false
+		switch I := in.(type) {
+		case string:
+			s = I
+			isString = true
+		case *string:
+			s = *I
+			isString = true
+		case int:
+		case *int:
+		default:
+		}
+		if isString {
+			_ = s
+		} else {
+
+		}
+		_ = s
 		return false
 	},
-	Bool: func(*Row, interface{}) bool {
+	Bool: func(r *Row, in interface{}) bool {
+		var s string
+		isString := false
+		switch I := in.(type) {
+		case string:
+			s = I
+			isString = true
+		case *string:
+			s = *I
+			isString = true
+		case bool:
+		case *bool:
+		default:
+		}
+		if isString {
+			_ = s
+		} else {
+
+		}
+		_ = s
 		return false
 	},
-	Int: func(*Row, interface{}) bool {
+	Int: func(r *Row, in interface{}) bool {
+		var s string
+		isString := false
+		switch I := in.(type) {
+		case string:
+			s = I
+			isString = true
+		case *string:
+			s = *I
+			isString = true
+		case int:
+		case *int:
+		default:
+		}
+		if isString {
+			_ = s
+		} else {
+
+		}
+		_ = s
 		return false
 	},
-	Tag: func(*Row, interface{}) bool {
+	Tag: func(r *Row, in interface{}) bool {
+		var s string
+		switch I := in.(type) {
+		case string:
+			s = I
+		case *string:
+			s = *I
+		default:
+		}
+		_ = s
 		return false
 	},
-	Tags: func(*Row, interface{}) bool {
+	Tags: func(r *Row, in interface{}) bool {
+		var s string
+		isString := false
+		switch I := in.(type) {
+		case string:
+			s = I
+			isString = true
+		case *string:
+			s = *I
+			isString = true
+		case []string:
+		case *[]string:
+		default:
+		}
+		if isString {
+			_ = s
+		} else {
+
+		}
+		_ = s
 		return false
 	},
-	Addr: func(*Row, interface{}) bool {
+	Addr: func(r *Row, in interface{}) bool {
+		var s string
+		switch I := in.(type) {
+		case string:
+			s = I
+		case *string:
+			s = *I
+		default:
+		}
+		_ = s
 		return false
 	},
-	Addrs: func(*Row, interface{}) bool {
+	Addrs: func(r *Row, in interface{}) bool {
+		var s string
+		isString := false
+		switch I := in.(type) {
+		case string:
+			s = I
+			isString = true
+		case *string:
+			s = *I
+			isString = true
+		case []string:
+		case *[]string:
+		default:
+		}
+		if isString {
+			_ = s
+		} else {
+
+		}
+		_ = s
 		return false
 	},
-	Algo: func(*Row, interface{}) bool {
+	Algo: func(r *Row, in interface{}) bool {
+		var s string
+		switch I := in.(type) {
+		case string:
+			s = I
+		case *string:
+			s = *I
+		default:
+		}
+		_ = s
 		return false
 	},
-	Float: func(*Row, interface{}) bool {
+	Float: func(r *Row, in interface{}) bool {
+		var s string
+		isString := false
+		switch I := in.(type) {
+		case string:
+			s = I
+			isString = true
+		case *string:
+			s = *I
+			isString = true
+		case float64:
+		case *float64:
+		default:
+		}
+		if isString {
+			_ = s
+		} else {
+
+		}
+		_ = s
 		return false
 	},
-	Duration: func(*Row, interface{}) bool {
+	Duration: func(r *Row, in interface{}) bool {
+		var s string
+		isString := false
+		switch I := in.(type) {
+		case string:
+			s = I
+			isString = true
+		case *string:
+			s = *I
+			isString = true
+		case time.Duration:
+		case *time.Duration:
+		default:
+		}
+		if isString {
+			_ = s
+		} else {
+
+		}
+		_ = s
 		return false
 	},
-	Net: func(*Row, interface{}) bool {
+	Net: func(r *Row, in interface{}) bool {
+		var s string
+		switch I := in.(type) {
+		case string:
+			s = I
+		case *string:
+			s = *I
+		default:
+		}
+		_ = s
 		return false
 	},
-	Level: func(*Row, interface{}) bool {
+	Level: func(r *Row, in interface{}) bool {
+		var s string
+		switch I := in.(type) {
+		case string:
+			s = I
+		case *string:
+			s = *I
+		default:
+		}
+		_ = s
 		return false
 	},
 }
