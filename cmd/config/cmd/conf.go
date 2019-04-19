a4d1170c40b40992434d7e74435db9f71d647313
Loki Verloren <l0k18@protonmail.com>
2019-04-19 18:33:11 +0200

    two menus and exit implemented

--- cmd/config/cmd/conf.go
+++ cmd/config/cmd/conf.go
@@ -17,91 +17,124 @@ var runConf = func(args []string, tokens config.Tokens, app *config.App) int {
 	var tapp *tview.Application
 
 	// tapp pulls everything together to create the configuration interface
-	tapp = func() (out *tview.Application) {
-		out = tview.NewApplication()
-		return
-	}()
+	tapp = tview.NewApplication()
 
 	// titlebar tells the user what app they are using
-	titlebar = func() (out *tview.TextView) {
-		out = tview.NewTextView()
-		out.SetTextColor(tcell.ColorWhite).
-			SetText(menutitle).
-			Box.SetBackgroundColor(tcell.ColorDarkGreen)
-		return
-	}()
+	titlebar = tview.NewTextView()
+	titlebar.SetTextColor(tcell.ColorWhite).
+		SetText(menutitle).
+		Box.SetBackgroundColor(tcell.ColorDarkGreen)
 
 	// root is the canvas (the whole current terminal view)
-	root = func() (out *tview.Flex) {
-		out = tview.NewFlex()
-		out.SetDirection(tview.FlexRow)
-		return
-	}()
+	root = tview.NewFlex()
+	root.SetDirection(tview.FlexRow)
 
 	root.AddItem(titlebar, 1, 1, false)
 
 	rootmenu := tview.NewGrid()
 	rootmenu.SetColumns(19, -1)
 	catsmenu := tview.NewGrid()
-	catsmenu.SetColumns(12, -1)
+	catsmenu.SetColumns(11, -1)
 
 	roottable := tview.NewTable()
-	catstable := tview.NewTable()
 	filler := tview.NewBox()
 
+	// catsmenu.AddItem(catstable, 0, 0, 1, 1, 1, 1, false)
+	roottable.SetBackgroundColor(tcell.ColorDarkGreen)
 	roottable.SetBorders(false)
-	roottable.SetCellSimple(0, 0, " < back")
-	sortedkeys := app.Cats.GetSortedKeys()
-	padlen := 0
-	for _, x := range sortedkeys {
-		if padlen < len(x) {
-			padlen = len(x)
-		}
-	}
-	for i, x := range sortedkeys {
-		pl := padlen - len(x)
-		pad := strings.Repeat(" ", pl)
-		roottable.SetCellSimple(i+1, 0, "   "+x+pad+" > ")
-	}
+	roottable.SetCellSimple(0, 0, "<                ")
+	roottable.SetCellSimple(1, 0, "  run a server  >")
+	roottable.SetCellSimple(2, 0, "  configuration >")
 
 	roottable.SetSelectable(true, true)
 	roottable.GetCell(0, 0)
+	catstable := GetCatsTable(app)
+
+	runmenu := GetRunMenu()
+	// runmenu.SetColumns(11, -1)
+
 	roottable.SetSelectedFunc(func(y, x int) {
 		switch y {
 		case 0:
-			rootmenu.RemoveItem(catsmenu)
-			rootmenu.AddItem(filler, 0, 1, 1, 1, 1, 1, false)
-			tapp.SetFocus(catstable)
+			tapp.Stop()
+		case 1:
+			roottable.SetSelectable(false, false)
+			runmenu.SetSelectable(true, true)
+			runmenu.SetBackgroundColor(tcell.ColorDarkGreen)
+			roottable.SetBackgroundColor(tcell.ColorBlack)
+			tapp.SetFocus(runmenu)
+		case 2:
+			roottable.SetSelectable(false, false)
+			catstable.SetSelectable(true, true)
+			roottable.SetBackgroundColor(tcell.ColorBlack)
 			catstable.SetBackgroundColor(tcell.ColorDarkGreen)
-			roottable.SetSelectedStyle(tcell.ColorWhite, tcell.ColorDarkGreen, tcell.AttrDim)
-			catstable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrBold)
+			tapp.SetFocus(catstable)
 		}
 	})
-	catsmenu.AddItem(roottable, 0, 0, 1, 1, 1, 1, false)
-	catstable.SetBackgroundColor(tcell.ColorDarkGreen)
-	catstable.SetBorders(false)
-	catstable.SetCellSimple(0, 0, " < exit           ")
-	catstable.SetCellSimple(1, 0, "   run a server  >")
-	catstable.SetCellSimple(2, 0, "   configuration >")
-	catstable.SetSelectedFunc(func(y, x int) {
+
+	runmenu.SetSelectedFunc(func(y, x int) {
 		switch y {
 		case 0:
-			tapp.Stop()
-		case 2:
-			catstable.SetBackgroundColor(tcell.ColorBlack)
-			catstable.SetSelectedStyle(tcell.ColorWhite, tcell.ColorDarkGreen, tcell.AttrDim)
-			roottable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrBold)
 			rootmenu.RemoveItem(filler)
-			rootmenu.AddItem(catsmenu, 0, 1, 1, 1, 1, 1, false)
+			// rootmenu.RemoveItem(catstable)
+			// rootmenu.RemoveItem(runmenu)
+			rootmenu.AddItem(filler, 0, 1, 1, 1, 1, 1, false)
+			rootmenu.SetColumns(17, 13, -1)
+			roottable.SetSelectable(true, true)
+			runmenu.SetBackgroundColor(tcell.ColorBlack)
+			runmenu.SetSelectable(false, false)
 			roottable.SetBackgroundColor(tcell.ColorDarkGreen)
+			rootmenu.AddItem(filler, 0, 2, 1, 1, 1, 1, false)
 			tapp.SetFocus(roottable)
 		}
 	})
-	catstable.SetSelectable(true, true)
-	catstable.GetCell(0, 0)
 
-	rootmenu.AddItem(catstable, 0, 0, 1, 1, 1, 1, true)
+	catstable.SetSelectedFunc(func(y, x int) {
+		switch y {
+		case 0:
+			rootmenu.RemoveItem(filler)
+			// rootmenu.RemoveItem(catstable)
+			// rootmenu.RemoveItem(runmenu)
+			rootmenu.AddItem(filler, 0, 1, 1, 1, 1, 1, false)
+			rootmenu.SetColumns(17, 10, -1)
+			roottable.SetSelectable(true, true)
+			catstable.SetBackgroundColor(tcell.ColorBlack)
+			catstable.SetSelectable(false, false)
+			roottable.SetBackgroundColor(tcell.ColorDarkGreen)
+			rootmenu.AddItem(filler, 0, 2, 1, 1, 1, 1, false)
+			tapp.SetFocus(roottable)
+		}
+	})
+
+	roottable.SetSelectionChangedFunc(func(y, x int) {
+		// titlebar.SetText(fmt.Sprint("y", y, " x", x))
+		switch y {
+		case 1:
+			rootmenu.RemoveItem(filler)
+			rootmenu.RemoveItem(catstable)
+			rootmenu.AddItem(runmenu, 0, 1, 1, 1, 1, 1, true)
+			rootmenu.AddItem(filler, 0, 2, 1, 1, 1, 1, false)
+			rootmenu.SetColumns(17, 13, -1)
+		case 2:
+			rootmenu.RemoveItem(filler)
+			rootmenu.RemoveItem(runmenu)
+			rootmenu.AddItem(catstable, 0, 1, 1, 1, 1, 1, true)
+			rootmenu.AddItem(filler, 0, 2, 1, 1, 1, 1, false)
+			rootmenu.SetColumns(17, 10, -1)
+		default:
+			rootmenu.RemoveItem(filler)
+			rootmenu.RemoveItem(catstable)
+			rootmenu.RemoveItem(runmenu)
+			rootmenu.AddItem(filler, 0, 1, 1, 1, 1, 1, false)
+			rootmenu.SetColumns(17, -1)
+			// tapp.ForceDraw()
+		}
+	})
+
+	rootmenu.AddItem(roottable, 0, 0, 1, 1, 1, 1, true)
+	// rootmenu.AddItem(runmenu, 0, 1, 1, 1, 1, 1, true)
 	rootmenu.AddItem(filler, 0, 1, 1, 1, 1, 1, false)
+	rootmenu.SetColumns(17, -1)
 
 	root.AddItem(rootmenu, 0, 1, true)
 
@@ -110,4 +143,34 @@ var runConf = func(args []string, tokens config.Tokens, app *config.App) int {
 	}
 
 	return 0
+}
+
+func GetCatsTable(app *config.App) *tview.Table {
+	catstable := tview.NewTable()
+	catstable.SetBorders(false)
+	sortedkeys := app.Cats.GetSortedKeys()
+	padlen := 0
+	for _, x := range sortedkeys {
+		if padlen < len(x) {
+			padlen = len(x)
+		}
+	}
+	catstable.SetCellSimple(0, 0, "< ")
+	for i, x := range sortedkeys {
+		pad := strings.Repeat(" ", padlen-len(x))
+		catstable.SetCellSimple(i+1, 0, "  "+x+pad+" >")
+	}
+	catstable.SetSelectable(false, false)
+	return catstable
+}
+
+func GetRunMenu() (out *tview.Table) {
+	out = tview.NewTable()
+	out.SetBorders(false)
+	out.SetSelectable(false, false)
+	out.SetCellSimple(0, 0, "<            ")
+	out.SetCellSimple(1, 0, "  🌱 node   >")
+	out.SetCellSimple(2, 0, "  💵 wallet >")
+	out.SetCellSimple(3, 0, "  🐚 shell  >")
+	return
 }
