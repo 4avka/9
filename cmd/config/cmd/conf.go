33c4429b7ff71103323304a7235bdeaee5eeab9e
Loki Verloren <l0k18@protonmail.com>
2019-04-22 00:32:27 +0200

    starting to look more like it!

--- cmd/config/cmd/conf.go
+++ cmd/config/cmd/conf.go
@@ -74,22 +74,22 @@ func Run(args []string, tokens config.Tokens, app *config.App) int {
 			out.SetCellSimple(i+1, 0, "  "+x+pad+" >")
 		}
 		catstablewidth += 4
-		out.SetSelectedStyle(tcell.ColorWhite, tcell.ColorDarkGreen, tcell.AttrDim)
-		out.SetSelectable(true, true)
-		out.SetSelectedStyle(tcell.ColorWhite, tcell.ColorBlack, tcell.AttrDim)
+		out.SetSelectedStyle(tcell.ColorWhite, tcell.ColorDarkGreen, tcell.AttrDim).
+			SetSelectable(true, true).
+			SetSelectedStyle(tcell.ColorWhite, tcell.ColorBlack, tcell.AttrDim)
 		return
 	}()
 
 	runtable := func() (out *tview.Table) {
-		out = tview.NewTable()
-		out.SetBorders(false)
-		out.SetSelectable(false, false)
-		out.SetCellSimple(0, 0, "<            ")
-		out.SetCellSimple(1, 0, "  🌱 node   >")
-		out.SetCellSimple(2, 0, "  💵 wallet >")
-		out.SetCellSimple(3, 0, "  🐚 shell  >")
-		out.SetSelectedStyle(tcell.ColorWhite, tcell.ColorBlack, tcell.AttrDim)
-		out.SetSelectable(true, true)
+		out = tview.NewTable().
+			SetBorders(false).
+			SetSelectable(false, false).
+			SetCellSimple(0, 0, "<            ").
+			SetCellSimple(1, 0, "  🌱 node   >").
+			SetCellSimple(2, 0, "  💵 wallet >").
+			SetCellSimple(3, 0, "  🐚 shell  >").
+			SetSelectedStyle(tcell.ColorWhite, tcell.ColorBlack, tcell.AttrDim).
+			SetSelectable(true, true)
 		return
 	}()
 
@@ -98,8 +98,9 @@ func Run(args []string, tokens config.Tokens, app *config.App) int {
 		case 0:
 			tapp.Stop()
 		case 1:
-			runtable.SetBackgroundColor(tcell.ColorDarkGreen)
-			runtable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone)
+			runtable.
+				SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone).
+				SetBackgroundColor(tcell.ColorDarkGreen)
 			for i := runtable.GetRowCount(); i >= 0; i-- {
 				runtable.GetCell(i, 0).SetTextColor(tcell.ColorWhite)
 			}
@@ -111,21 +112,21 @@ func Run(args []string, tokens config.Tokens, app *config.App) int {
 				rtbl := roottable.GetCell(i, 0)
 				y, _ := roottable.GetSelection()
 				if i != y {
-					// SetTextColor(tcell.ColorBlack).
 					rtbl.SetAttributes(tcell.AttrDim)
 				}
 			}
-			roottable.SetBackgroundColor(tcell.ColorBlack)
-			roottable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorDarkGreen, tcell.AttrDim&tcell.AttrBold)
+			roottable.
+				SetSelectedStyle(tcell.ColorBlack, tcell.ColorDarkGreen, tcell.AttrDim&tcell.AttrBold).
+				SetBackgroundColor(tcell.ColorBlack)
 
 			tapp.SetFocus(runtable)
 		case 2:
-			catstable.SetBackgroundColor(tcell.ColorDarkGreen)
-			catstable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone)
+			catstable.
+				SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone).
+				SetBackgroundColor(tcell.ColorDarkGreen)
 			for i := catstable.GetRowCount(); i >= 0; i-- {
 				catstable.GetCell(i, 0).SetTextColor(tcell.ColorWhite)
 			}
-			catstable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrDim)
 
 			for i := roottable.GetRowCount(); i >= 0; i-- {
 				roottable.GetCell(i, 0).SetTextColor(tcell.ColorWhite)
@@ -138,8 +139,9 @@ func Run(args []string, tokens config.Tokens, app *config.App) int {
 					rtbl.SetAttributes(tcell.AttrDim)
 				}
 			}
-			roottable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorDarkGreen, tcell.AttrDim&tcell.AttrBold)
-			roottable.SetBackgroundColor(tcell.ColorBlack)
+			roottable.
+				SetSelectedStyle(tcell.ColorBlack, tcell.ColorDarkGreen, tcell.AttrDim&tcell.AttrBold).
+				SetBackgroundColor(tcell.ColorBlack)
 
 			tapp.SetFocus(catstable)
 		}
@@ -148,44 +150,48 @@ func Run(args []string, tokens config.Tokens, app *config.App) int {
 	roottable.SetSelectionChangedFunc(func(y, x int) {
 		switch y {
 		case 0:
-			rootgrid.RemoveItem(blackbox)
-			rootgrid.RemoveItem(catstable)
-			rootgrid.RemoveItem(runtable)
-			rootgrid.AddItem(blackbox, 0, 1, 1, 1, 1, 1, false)
-			rootgrid.SetColumns(rootgridwidth, -1)
+			rootgrid.
+				RemoveItem(blackbox).
+				RemoveItem(catstable).
+				RemoveItem(runtable).
+				AddItem(blackbox, 0, 1, 1, 1, 1, 1, false).
+				SetColumns(rootgridwidth, -1)
 
 			catstable.SetSelectedStyle(tcell.ColorWhite, tcell.ColorBlack, tcell.AttrDim)
 
 			runtable.SetSelectedStyle(tcell.ColorWhite, tcell.ColorBlack, tcell.AttrDim)
-			roottable.SetSelectedStyle(tcell.ColorOrange, tcell.ColorDarkGreen, tcell.AttrBold)
-			roottable.GetCell(0, 0).SetText("< exit")
-			// tapp.ForceDraw()
+			roottable.
+				SetSelectedStyle(tcell.ColorOrange, tcell.ColorDarkGreen, tcell.AttrBold).
+				GetCell(0, 0).SetText("< exit")
 		case 1:
-			rootgrid.RemoveItem(blackbox)
-			rootgrid.RemoveItem(catstable)
-			rootgrid.AddItem(runtable, 0, 1, 1, 1, 1, 1, true)
-			rootgrid.AddItem(blackbox, 0, 2, 1, 1, 1, 1, false)
-			rootgrid.SetColumns(rootgridwidth, runtablewidth, -1)
+			rootgrid.
+				RemoveItem(blackbox).
+				RemoveItem(catstable).
+				AddItem(runtable, 0, 1, 1, 1, 1, 1, true).
+				AddItem(blackbox, 0, 2, 1, 1, 1, 1, false).
+				SetColumns(rootgridwidth, runtablewidth, -1)
 
-			roottable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone)
 			for i := runtable.GetRowCount(); i >= 0; i-- {
 				runtable.GetCell(i, 0).SetTextColor(tcell.ColorDarkGreen)
 			}
 			runtable.SetSelectedStyle(tcell.ColorDarkGreen, tcell.ColorBlack, tcell.AttrDim)
-			roottable.GetCell(0, 0).SetText("<")
+			roottable.
+				SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone).
+				GetCell(0, 0).SetText("<")
 		case 2:
-			rootgrid.RemoveItem(blackbox)
-			rootgrid.RemoveItem(runtable)
-			rootgrid.AddItem(catstable, 0, 1, 1, 1, 1, 1, true)
-			rootgrid.AddItem(blackbox, 0, 2, 1, 1, 1, 1, false)
-			rootgrid.SetColumns(rootgridwidth, catstablewidth, -1)
+			rootgrid.RemoveItem(blackbox).
+				RemoveItem(runtable).
+				AddItem(catstable, 0, 1, 1, 1, 1, 1, true).
+				AddItem(blackbox, 0, 2, 1, 1, 1, 1, false).
+				SetColumns(rootgridwidth, catstablewidth, -1)
 
-			roottable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone)
 			for i := catstable.GetRowCount(); i >= 0; i-- {
 				catstable.GetCell(i, 0).SetTextColor(tcell.ColorDarkGreen)
 			}
 			catstable.SetSelectedStyle(tcell.ColorDarkGreen, tcell.ColorBlack, tcell.AttrDim)
-			roottable.GetCell(0, 0).SetText("<")
+			roottable.
+				SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone).
+				GetCell(0, 0).SetText("<")
 		}
 	})
 
@@ -194,8 +200,9 @@ func Run(args []string, tokens config.Tokens, app *config.App) int {
 		rootgrid.SetColumns(rootgridwidth, width, -1)
 		// rootgrid.AddItem(blackbox, 0, 2, 1, 1, 1, 1, false)
 
-		table.SetBackgroundColor(tcell.ColorBlack)
-		table.SetSelectedStyle(tcell.ColorWhite, tcell.ColorBlack, tcell.AttrDim)
+		table.
+			SetSelectedStyle(tcell.ColorWhite, tcell.ColorBlack, tcell.AttrDim).
+			SetBackgroundColor(tcell.ColorBlack)
 		for i := table.GetRowCount(); i >= 0; i-- {
 			table.GetCell(i, 0).SetTextColor(tcell.ColorDarkGreen)
 		}
@@ -204,9 +211,10 @@ func Run(args []string, tokens config.Tokens, app *config.App) int {
 		for i := roottable.GetRowCount(); i >= 0; i-- {
 			roottable.GetCell(i, 0).SetTextColor(tcell.ColorWhite).SetAttributes(tcell.AttrNone)
 		}
+		roottable.
+			SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone).
+			SetBackgroundColor(tcell.ColorDarkGreen)
 		roottable.GetCell(0, 0).SetText("<")
-		roottable.SetBackgroundColor(tcell.ColorDarkGreen)
-		roottable.SetSelectedStyle(tcell.ColorBlack, tcell.ColorWhite, tcell.AttrNone)
 
 		tapp.SetFocus(roottable)
 	}
@@ -231,9 +239,15 @@ func Run(args []string, tokens config.Tokens, app *config.App) int {
 	catstable.SetSelectionChangedFunc(func(y, x int) {
 	})
 
-	root.AddItem(rootgrid.AddItem(roottable, 0, 0, 1, 1, 1, 1, true).
-		AddItem(blackbox, 0, 1, 1, 1, 1, 1, false).
-		SetColumns(rootgridwidth, -1), 0, 1, true)
+	root.AddItem(
+		rootgrid.
+			AddItem(
+				roottable, 0, 0, 1, 1, 1, 1, true).
+			AddItem(
+				blackbox, 0, 1, 1, 1, 1, 1, false).
+			SetColumns(
+				rootgridwidth, -1),
+		0, 1, true)
 
 	if e := tapp.SetRoot(root, true).Run(); e != nil {
 		panic(e)
