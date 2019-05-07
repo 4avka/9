7c97eddd4c7f9facb0f74554b55a1aee0e967784
Loki Verloren <l0k18@protonmail.com>
2019-05-07 12:10:52 +0200

    moved fek into bola

--- pkg/bola/fec_test.go
+++ pkg/bola/fec_test.go
@@ -1,9 +1,53 @@
-package fek
+package bola
 
 import (
+	"crypto/rand"
+	"encoding/binary"
 	"encoding/hex"
 	"testing"
 )
+
+func TestCodec(t *testing.T) {
+	const (
+		requiredshards = 3
+		totalshards    = 9
+	)
+	testData := make([]byte, 64)
+	_, _ = rand.Read(testData)
+	t.Log(len(testData), testData)
+	rsc := New(requiredshards, totalshards)
+	encoded := rsc.Encode(testData)
+	for _, x := range encoded {
+		t.Log(len(x), x)
+		uuidb := x[:4]
+		shardnum := x[4]
+		payload := x[5:]
+		t.Log(binary.LittleEndian.Uint32(uuidb), shardnum, payload)
+	}
+	muddlemap := make(map[int][]byte)
+	for i, x := range encoded {
+		muddlemap[i] = x
+	}
+	// By copying the array into a slice and reading back out we unsort it
+	var muddled [][]byte
+	for _, x := range muddlemap {
+		muddled = append(muddled, x)
+	}
+	for i := range muddled {
+		// t.Log(i, len(x), x)
+		if i%2 == 0 || i%3 == 0 {
+			muddled[i][3] = ^muddled[i][3]
+			// t.Log(i, len(x), x, "MANG")
+		} else {
+			// t.Log(i, len(x), x)
+		}
+	}
+	unmuddled := rsc.Decode(muddled)
+	t.Log(len(unmuddled), unmuddled)
+	if string(unmuddled) != string(testData) {
+		t.Fatal("encode/decode failed")
+	}
+}
 
 var (
 	testDataAligned   = []byte("123456789123456789123456789123456789123456789123456789123456789123456789123456789")
@@ -32,4 +76,14 @@ func TestPadData(t *testing.T) {
 	blowout := PadData(make([]byte, 65536*10), 3, 9)
 	t.Log(blowout)
 	t.Log(UnpadData(PadData(testDataUnaligned, 3, 9)[:10]))
+}
+
+func TestChecksum(t *testing.T) {
+	test := []byte("hello world!")
+	t.Log(string(test))
+	t.Log(test)
+	checked := AppendChecksum(test)
+	t.Log(checked)
+	verified, ok := VerifyChecksum(checked)
+	t.Log(ok, verified)
 }
